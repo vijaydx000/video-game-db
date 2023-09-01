dc7c80e4d2a6eb3c71b56c9b63b676103699d646
Vijay <fastestvijay39@gmail.com>
2023-09-01 15:07:18 +0530

    third commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  public sort: string | undefined;
+  public games: Array<Game> | undefined;
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-serach']) {
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpService
+       .getGameList(sort, search)
+       .subscribe((gameList: APIResponse<Game>) => {
+        this.games = gameList.results;
+        console.log(gameList);
+       });
+  }
+
+}
---
+++ src/app/interceptors/http-headers..interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'c149e6fff2msh4826735826e8117p17770ajsnd1f8994a99b9',
+        'x-rapidapi-host': 'demo-project49379.p.rapidapi.com',
+      },
+      setParams: {
+        key: 'e40e743af2c94b0c916a8aa618fb4473',
+      }
+    });
+    return next.handle(req);
+  }
+}
